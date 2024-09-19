1dda7f7e135a834368fea7a82de2c608509d42f0
willy0483 <willyjensen251@gmail.com>
2024-09-19 14:26:44 +0200

    ok

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,3 +1,4 @@
+//#region VARIABLES
 let categories = []; // Store categories
 let mainCategories = {
   Beauty: [],
@@ -8,10 +9,16 @@ let mainCategories = {
 };
 
 let myApp = document.getElementById("app");
+let headerDiv = document.createElement("header");
+myApp.appendChild(headerDiv);
+headerDiv.classList.add("headerNav");
 
+let searchInput = document.getElementById("searchInput");
 let productsDiv = document.createElement("div"); // Div for displaying products
 let productsContainer = document.createElement("div");
+//#endregion
 
+//#region DATA FETCHING AND HANDLING
 getData(); // Fetch data
 
 // Fetch data from the new category list API
@@ -20,7 +27,6 @@ function getData() {
     .then((response) => {
       if (!response.ok) throw new Error(`HTTP error: ${response.status}`);
       console.log(response);
-
       return response.json();
     })
     .then((data) => {
@@ -34,21 +40,6 @@ function getData() {
       console.error(error);
       buildError(error);
     });
-}
-
-function buildError(error) {
-  myApp.innerHTML = "";
-
-  myHtml = `
-
-  <div class="error-container">
-    <h1>Error loading data</h1>
-    <p>Please try again.</p>
-    <p>${error}</p>
-  </div>
- `;
-
-  myApp.innerHTML = myHtml;
 }
 
 // Sort categories into main and subcategories
@@ -98,31 +89,99 @@ function sortCategories(data) {
   createButton(mainCategories); // Create buttons for categories
 }
 
+// Build error message
+function buildError(error) {
+  myApp.innerHTML = "";
+
+  myHtml = `
+    <div class="error-container">
+      <h1>Error loading data</h1>
+      <p>Please try again.</p>
+      <p>${error}</p>
+    </div>
+   `;
+
+  myApp.innerHTML = myHtml;
+}
+
+// Display products from category
+function displayProducts(subCategories) {
+  productsDiv.innerHTML = ""; // Clear previous products
+  productsContainer.innerHTML = ""; // Clear previous products container
+  productsContainer.classList.add("products");
+
+  subCategories.forEach((subCategory) => {
+    let url = `https://dummyjson.com/products/category/${subCategory}`;
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Error: " + response.status);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        let myHtml = "";
+        // Loop through each product
+
+        if (Array.isArray(data.products)) {
+          data.products.forEach((categoriesProducts) => {
+            // Generate HTML for each product
+            myHtml += `
+                <figure>
+                  <img src="${categoriesProducts.thumbnail}" alt="${
+              categoriesProducts.title
+            }">
+                  <figcaption>
+                    <h3>${categoriesProducts.title}</h3>
+                    <p>${categoriesProducts.price} $</p>
+                    <div class="rating">${createStars(
+                      categoriesProducts.rating
+                    )}</div>
+                  </figcaption>
+                </figure>
+              `;
+          });
+
+          // Insert products into the container
+          productsDiv.innerHTML = myHtml;
+          productsContainer.appendChild(productsDiv);
+          myApp.appendChild(productsContainer);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        buildError(error);
+      });
+  });
+}
+//#endregion
+
+//#region UI FUNCTIONS
 // Create category buttons and subcategories
 function createButton(data) {
   let buttonDiv = document.createElement("section");
-  myApp.appendChild(buttonDiv);
-
+  buttonDiv.classList.add("category");
   let myHtml = "";
 
   for (let mainCategory in data) {
     myHtml += `
-      <div class="category-wrapper">
-        <button class="category-button" data-category="${mainCategory}">
-          ${mainCategory}
-        </button>
-        <div class="subcategories" id="${mainCategory}-subcategories">
-          ${data[mainCategory]
-            .map(
-              (sub) =>
-                `<div class="subcategory" onclick="handleSubcategoryClick('${sub}')">${sub}</div>`
-            )
-            .join("")}
-        </div>
-      </div>`;
+        <div class="category-wrapper">
+          <button class="category-button" data-category="${mainCategory}">
+            ${mainCategory}
+          </button>
+          <div class="subcategories" id="${mainCategory}-subcategories">
+            ${data[mainCategory]
+              .map(
+                (sub) =>
+                  `<div class="subcategory" onclick="handleSubcategoryClick('${sub}')">${sub}</div>`
+              )
+              .join("")}
+          </div>
+        </div>`;
   }
 
   buttonDiv.innerHTML = myHtml;
+  headerDiv.prepend(buttonDiv);
 
   // Add event listeners for hover effects
   document.querySelectorAll(".category-wrapper").forEach((wrapper) => {
@@ -152,59 +211,6 @@ function handleSubcategoryClick(subCategory) {
   displayProducts([subCategory]);
 }
 
-// Display products from category
-function displayProducts(subCategories) {
-  productsDiv.innerHTML = ""; // Clear previous products
-  productsContainer.innerHTML = ""; // Clear previous products container
-  productsContainer.classList.add("products");
-
-  subCategories.forEach((subCategory) => {
-    let url = `https://dummyjson.com/products/category/${subCategory}`;
-    fetch(url)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Error: " + response.status);
-        }
-        return response.json();
-      })
-      .then((data) => {
-        let myHtml = "";
-        // Loop through each product
-
-        if (Array.isArray(data.products)) {
-          data.products.forEach((categoriesProducts) => {
-            // Generate HTML for each product
-            myHtml += `
-              <figure>
-                <img src="${categoriesProducts.thumbnail}" alt="${
-              categoriesProducts.title
-            }">
-                <figcaption>
-                  <h3>${categoriesProducts.title}</h3>
-                  <p>${categoriesProducts.price} $</p>
-                  <div class="rating">${createStars(
-                    categoriesProducts.rating
-                  )}</div>
-                </figcaption>
-              </figure>
-            `;
-          });
-
-          // Insert products into the container
-          productsDiv.innerHTML = myHtml;
-          productsContainer.appendChild(productsDiv);
-          myApp.appendChild(productsContainer);
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-        buildError(error);
-      });
-  });
-}
-
-// buildError(error)
-
 // Create stars based on product rating
 function createStars(rating) {
   let stars = "";
@@ -213,14 +219,16 @@ function createStars(rating) {
   // Loop to create a 5-star system
   for (let i = 0; i < 5; i++) {
     if (i < rating) {
-      stars += `<img class="star" src="assets/images/Svg/Filled star.svg" alt="Filled star">`; // Filled star
+      stars += `<img class="star" src="assets/images/svg/Filled star.svg" alt="Filled star">`; // Filled star
     } else {
-      stars += `<img class="star" src="assets/images/Svg/Empty star.svg" alt="Empty star">`; // Empty star
+      stars += `<img class="star" src="assets/images/svg/Empty star.svg" alt="Empty star">`; // Empty star
     }
   }
   return stars;
 }
+//#endregion
 
+//#region FOOTER
 // Function to create and add the footer
 function buildFooter() {
   // Create the footer element
@@ -229,38 +237,38 @@ function buildFooter() {
 
   // Set the innerHTML of the footer
   footer.innerHTML = `
-    <div class="lists">
-      <ul>
-        <h3>Company</h3>
-        <p>Infenite Finds Emv</p>
-        <p>CVR-NB-44215799</p>
-      </ul>
-      <ul>
-        <h3>Address</h3>
-        <p>Bakkegårdsvej 28 C,2</p>
-        <p>9000 Aalborg</p>
-      </ul>
-      <ul>
-        <h3>Policy</h3>
-        <li><a href="#">Terms and Conditions</a></li>
-        <li><a href="#">Privacy</a></li>
-        <li><a href="#">Withdrawal</a></li>
-        <li><a href="#">Cookie's</a></li>
-      </ul>
-      <ul>
-        <h3>Contact Info</h3>
-        <p>Phone: +45 42506072</p>
-        <p>E-mail: Infinite@finds.dk</p>
-      </ul>
-    <div class="ratings">
-      <ul>
-        <li><img src="assets/Images/Loading/Link [is-br-12--is-m-br-10--slide-in].svg" alt=""></li>
-        <li><img src="assets/Images/Loading/Link [footer__ben-item_one].svg" alt=""></li>
-        <li><img src="assets/Images/Loading/Link [footer__ben-item].svg" alt=""></li>
-      </ul>
-          </div>
-    </div>
-  `;
+      <div class="lists">
+        <ul>
+          <h3>Company</h3>
+          <p>Infenite Finds Emv</p>
+          <p>CVR-NB-44215799</p>
+        </ul>
+        <ul>
+          <h3>Address</h3>
+          <p>Bakkegårdsvej 28 C,2</p>
+          <p>9000 Aalborg</p>
+        </ul>
+        <ul>
+          <h3>Policy</h3>
+          <li><a href="#">Terms and Conditions</a></li>
+          <li><a href="#">Privacy</a></li>
+          <li><a href="#">Withdrawal</a></li>
+          <li><a href="#">Cookie's</a></li>
+        </ul>
+        <ul>
+          <h3>Contact Info</h3>
+          <p>Phone: +45 42506072</p>
+          <p>E-mail: Infinite@finds.dk</p>
+        </ul>
+      <div class="ratings">
+        <ul>
+          <li><img src="assets/images/svg/Link [footer__ben-item1].svg" alt=""></li>
+          <li>    <img src="assets/images/svg/Link [footer__ben-item].svg" alt=""></li>
+          <li><img src="assets/images/svg/Link [is-br-12--is-m-br-10--slide-in1].svg" alt=""></li>
+        </ul>
+            </div>
+      </div>
+    `;
 
   // Append the footer to the body
   document.body.appendChild(footer);
@@ -268,3 +276,39 @@ function buildFooter() {
 
 // Call the function to build and add the footer to the document
 buildFooter();
+buildHeader();
+
+//#endregion
+function buildHeader() {
+  let logoDiv = document.createElement("div");
+  logoDiv.classList.add("logo");
+
+  let logoHtml = `
+    <img onclick="resetView()" src="assets/images/Svg/Header.svg" alt="logo" class="header-logo">
+  `;
+
+  logoDiv.innerHTML = logoHtml;
+  headerDiv.appendChild(logoDiv);
+
+  const searchInputHTML = `
+  <div class="search">
+  <img src="assets/images/Svg/Search-icon .svg" alt="search-logo" />
+  <input id="searchInput" type="text"  placeholder="Search products..." />
+  </div>
+`;
+
+  headerDiv.innerHTML += searchInputHTML;
+
+  searchInput = document.getElementById("searchInput");
+  searchInput.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+      let searchQuery = event.target.value;
+      displayProducts(searchQuery);
+    }
+  });
+}
+
+function resetView() {
+  productsDiv.innerHTML = "";
+  productsContainer.innerHTML = "";
+}
